6c75deeac0a80cd057b1a070a42d1489c33977fb
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2022-02-10 01:07:01 +0530

    commit for router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Customers from "./components/customers";
+import MovieForm from "./components/movieForm";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to={"/movies"} />
+          <Redirect to={"/not-found"} />
+        </Switch>
+      </main>
+    </>
   );
 }
 
